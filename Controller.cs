a7f6b2bf155415a36ff71a2f075c8d8a2619b4d3
ElaineGilstrom <elainegilstrom@gmail.com>
2018-03-18 00:41:57 -0700

    added in exporting for .obj formats, but it is still bugged to hell and back.

--- Controller.cs
+++ Controller.cs
@@ -16,18 +16,19 @@ public class Controller : MonoBehaviour {
 	
 	void Update () {
 		if(!lastPos.Equals(Input.mousePosition)) {
-			if(Input.GetMouseButton(1) && !Input.GetMouseButton(0) && !Input.GetMouseButton(2)) {
+			if(!Input.GetMouseButton(1) && !Input.GetMouseButton(0) && Input.GetMouseButton(2)) {
 				var forward = cam.transform.forward;
 				forward.x = 0f; forward.y = 0f;
 				forward.Normalize();
 				cam.transform.Translate(forward * (lastPos - Input.mousePosition)[1] * scrollSpeed * -1);
-			} else if(!Input.GetMouseButton(1) && !Input.GetMouseButton(0) && Input.GetMouseButton(2)) {
+			} else if(!Input.GetMouseButton(1) && Input.GetMouseButton(0) && !Input.GetMouseButton(2)) {
 				var right = cam.transform.right;
 				right.y = 0f; right.Normalize();
 				var up = cam.transform.up;
+				up.x = 0f; up.Normalize();
 				Vector3 dif = (right * (lastPos - Input.mousePosition)[0] + up * (lastPos - Input.mousePosition)[1]) * scrollSpeed;
 				cam.transform.Translate(dif);
-			} else if(!Input.GetMouseButton(1) && Input.GetMouseButton(0) && !Input.GetMouseButton(2)) {
+			} else if(Input.GetMouseButton(1) && !Input.GetMouseButton(0) && !Input.GetMouseButton(2)) {
 				Vector3 dif = (lastPos - Input.mousePosition) * sensitivity;
 				cam.transform.Rotate(new Vector3(dif[1], -1 * dif[0], 0));
 			}
--- Generator.cs
+++ Generator.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System;
+using System.Text;
+
 public class Generator : MonoBehaviour {
 
 	enum face {
@@ -134,10 +138,103 @@ public class Generator : MonoBehaviour {
 	public void updateFileName(string n) { expFileName = n; }
 
 	public void exportOBJ() {
+		List<Vector3> v = new List<Vector3>();//verticies are xyz coordinates
+		List<int[]> f = new List<int[]>();//faces are 4 indxs of verticies in list
+		genVFLists(ref v, ref f);
+		
+		FileStream of = File.Open(expFileName + ".obj", FileMode.Create);
 
+		foreach(Vector3 vec in v) {
+			string w = "v " + vec.x.ToString() + " " + vec.y.ToString() + " " + vec.z.ToString() + System.Environment.NewLine;
+			byte[] b = strToByte(w);
+			of.Write(b, 0, b.Length);
+		}
+
+		foreach(int[] face in f) {
+			string w = "f";
+			foreach(int i in face) w += " " + i.ToString();
+			w += System.Environment.NewLine;
+			byte[] b = strToByte(w);
+			of.Write(b, 0, b.Length);
+		}
+
+		of.Close();
+	}
+
+	byte[] strToByte(string s) {
+		char[] c = s.ToCharArray();
+		byte[] b = new byte[c.Length];
+		for(int i = 0; i < c.Length; i ++) b[i] = (byte) c[i];
+		return b;
 	}
 
 	public void exportMap() {
 			
+	}
+
+	private void genVFLists(ref List<Vector3> v, ref List<int[]> f) {
+		foreach(Transform c in parent) {
+			var pos = c.position;
+			var size = c.GetComponent<Renderer>().bounds.size;
+			int[][] faces = new int[6][];
+			for(int i = 0; i < faces.Length; i++) faces[i] = new int[4];
+			Vector3 vec = pos;
+			if(!v.Contains(vec)) { v.Add(vec); Debug.Log("Added vec " + vecToStr(vec)); }
+
+			int fn = find(v, vec);
+			faces[0][0] = fn; faces[1][0] = fn; faces[2][0] = fn;
+
+			for(int i = 0; i < 3; i++) {
+				vec = pos;
+				vec[i] += size[i];
+				if(!v.Contains(vec)) { v.Add(vec); Debug.Log("Added vec " + vecToStr(vec)); }
+				fn = find(v, vec);
+				faces[i + 3][2] = fn; faces[i][1] = fn; faces[(2 + i) % 3][3] = fn;
+				bool oneSet = false;
+				for(int j = 0; j < 3; j++) {
+					if(j == i) continue;
+					vec[j] += size[i];
+					if(!v.Contains(vec)) { v.Add(vec); Debug.Log("Added vec " + vecToStr(vec)); }
+					fn = find(v, vec);
+					if(oneSet) faces[i + 3][3] = fn;
+					else { faces[i + 3][1] = fn; oneSet = true; }
+					if((2 + i) % 3 == j) faces[i][2] = fn;
+				}
+			}
+			vec = pos + size;
+			if(!v.Contains(vec)) { v.Add(vec); Debug.Log("Added vec " + vecToStr(vec)); }
+
+			fn = find(v, vec);
+			faces[3][0] = fn; faces[4][0] = fn; faces[5][0] = fn;
+
+			foreach(int[] fa in faces) {
+				if(!intArrInList(f, fa)) { f.Add(fa); Debug.Log("Adding face: " + fa.ToString()); }
+			}
+		}
+	}
+
+	private int find(List<Vector3> lst, Vector3 vec) {
+		for(int i = 0; i < lst.Count; i++) {
+			if(lst[i] == vec) return i + 1;
+		}
+		return -1;
+	}
+
+	private bool intArrInList(List<int[]> l, int[] f) {
+		foreach(int[] a in l) {
+			if(a.Length != f.Length) continue;
+			bool c = false;
+			for(int i = 0; i < f.Length; i++) if(a[i] != f[i]) { c = true; break; }
+			if(c) continue;
+			return true;
+		}
+		return false;
+	}
+
+	private string vecToStr(Vector3 v) {
+		string r = "{";
+		for(int i = 0; i < 3; i++) r += " " + v[i].ToString();
+		r += " }";
+		return r;
 	}
 }
